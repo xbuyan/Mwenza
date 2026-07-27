f16bb8dbef1817d9046f44312c16d4562151bc9d
xbuyan <kevobuyan@gmail.com>
2026-07-28 00:48:13 +0300

    feat(product): add product constructor

--- internal/domain/product/errors.go
+++ internal/domain/product/errors.go
@@ -3,6 +3,7 @@ package product
 import "errors"
 
 var (
+	ErrEmptyID     = errors.New("product id cannot be empty")
 	ErrEmptyName   = errors.New("product name cannot be empty")
 	ErrEmptySKU    = errors.New("product SKU cannot be empty")
 	ErrInvalidUnit = errors.New("invalid product unit")
--- internal/domain/product/product.go
+++ internal/domain/product/product.go
@@ -7,3 +7,29 @@ type Product struct {
 	unit   Unit
 	status Status
 }
+
+func New(id string, sku SKU, name string, unit Unit) (*Product, error) {
+	if id == "" {
+		return nil, ErrEmptyID
+	}
+
+	if sku == "" {
+		return nil, ErrEmptySKU
+	}
+
+	if name == "" {
+		return nil, ErrEmptyName
+	}
+
+	if unit == "" {
+		return nil, ErrInvalidUnit
+	}
+
+	return &Product{
+		id:     id,
+		sku:    sku,
+		name:   name,
+		unit:   unit,
+		status: StatusActive,
+	}, nil
+}
